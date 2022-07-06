0243bc3e5d1eb77720f23191de7f64d5b09748f8
Prathamesh <prathamesh@leewayhertz.com>
2022-07-06 17:22:10 +0530

    Added JWT API route to main.rs

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,7 @@ pub fn init(config: &mut web::ServiceConfig) {
             .service(routes::update_user)
             .service(routes::get_all_users)
             .service(routes::delete_user)
+            .service(routes::create_jwt_token)
             .service(routes::get_user),
     );
 }
