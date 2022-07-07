7099baac0acefd14f04c2300dbd7748386211da6
lhtPrathamesh <105263884+lhtPrathamesh@users.noreply.github.com>
2022-07-07 15:47:28 +0530

    Update index.rs

--- src/app/constants/index.rs
+++ src/app/constants/index.rs
@@ -1,3 +1,3 @@
-pub const DB_NAME: &str = "rust-dev";
-pub const USER_COLLECTION: &str = "new-users";
-pub const DB_URL: &str = "mongodb+srv://lhtPrathamesh:4fSxQUptob9lv8xy@cluster0.xv2wk.mongodb.net/?retryWrites=true&w=majority";
+pub const DB_NAME: &str = "";
+pub const USER_COLLECTION: &str = "";
+pub const DB_URL: &str = "";
